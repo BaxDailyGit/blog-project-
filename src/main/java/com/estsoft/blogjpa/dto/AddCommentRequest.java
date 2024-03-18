c48ad411df5ca4bd0c8fcdb0cdc7e9831fe83717
BaekSeungJin_백승진 <tmdwls8387@ajou.ac.kr>
2024-03-18 14:45:38 +0900

    AddCommentRequest dto 추가

---
+++ src/main/java/com/estsoft/blogjpa/dto/AddCommentRequest.java
@@ -0,0 +1,12 @@
+package com.estsoft.blogjpa.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class AddCommentRequest {
+    private String content;
+}
