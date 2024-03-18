dc613294ee704977e87aab90ae45beeb238b9bd9
BaekSeungJin_백승진 <tmdwls8387@ajou.ac.kr>
2024-03-18 14:43:54 +0900

    Comment 엔티티 추가

---
+++ src/main/java/com/estsoft/blogjpa/domain/Comment.java
@@ -0,0 +1,33 @@
+package com.estsoft.blogjpa.domain;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "article_id", nullable = false)
+    private Article article;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+    }
+}
